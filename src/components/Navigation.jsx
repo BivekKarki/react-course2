88dfa3e129edc20f5907b2182884caa7b2ed8a5b
BiveKKarkii <np03cs4a210046@heraldcollege.edu.np>
2023-07-31 22:28:12 +0545

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <div className="container">
+        <nav>
+            <div className="logo">
+                <img src="/images/logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
+
